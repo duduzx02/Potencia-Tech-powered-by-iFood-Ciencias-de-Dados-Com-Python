f878a0a3c541ffafd5c0ebc617b962d271d55164
duduzx02 <duduzx02@gmail.com>
2023-07-24 00:49:46 -0300

    range com for

---
+++ DominandoOPythonParaCienciaDeDados/EstruturasCondicionaisERepeticao/rangeComFor.py
@@ -0,0 +1,8 @@
+for numero in range(0, 11):
+    print(numero, end=" ")
+print()
+for numero in range(0, 31, 3):
+    print(numero, end=" ")
+print()
+for numero in range(0, 71, 7):
+    print(numero, end=" ")

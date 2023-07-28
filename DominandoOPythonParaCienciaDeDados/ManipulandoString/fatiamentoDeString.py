f7b4f1e97d7913a538fc1fd195202c1824c1a67a
duduzx02 <duduzx02@gmail.com>
2023-07-28 03:23:35 -0300

    Fatiamento de String

---
+++ DominandoOPythonParaCienciaDeDados/ManipulandoString/fatiamentoDeString.py
@@ -0,0 +1,10 @@
+nome = "Eduardo de Souza Lima"
+
+print(nome[0].strip())
+print(nome[-1].strip())
+print(nome[:9].strip())
+print(nome[9:].strip())
+print(nome[10:16].strip())
+print(nome[10:18:2].strip())
+print(nome[:].strip())
+print(nome[::-1].strip())

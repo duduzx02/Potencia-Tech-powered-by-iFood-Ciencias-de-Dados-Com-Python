97047f9537c34d178576930d3bd6e6dc1701309c
duduzx02 <duduzx02@gmail.com>
2023-07-28 02:32:05 -0300

    Metodos úteis

---
+++ DominandoOPythonParaCienciaDeDados/ManipulandoString/MetodosUteis.py
@@ -0,0 +1,13 @@
+curso = "   pYtHon  "
+cursoPython = "Curso de Python"
+print(curso.upper()) # Tudo maiúsculo
+print(curso.lower()) # Tudo minúsculo
+print(curso.title()) # Apenas a primeira letra maiúsculo
+
+print(curso.strip()) # Elimina os espaços 
+print(curso.lstrip()) # Elimina os espaços da esquerda
+print(curso.rstrip()) # Elimina os espaços da direita
+
+print(curso.center(25, "*"))
+print(".".join(curso))
+print(".".join(cursoPython))

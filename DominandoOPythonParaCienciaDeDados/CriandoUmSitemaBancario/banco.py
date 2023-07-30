2e9ffe635a0eaa090e98275329fff082a0e703e4
duduzx02 <duduzx02@gmail.com>
2023-07-30 02:57:13 -0300

    Refatoração do código

--- DominandoOPythonParaCienciaDeDados/CriandoUmSitemaBancario/banco.py
+++ DominandoOPythonParaCienciaDeDados/CriandoUmSitemaBancario/banco.py
@@ -20,25 +20,33 @@ while true:
 
     elif opcao == 1:
         deposito = int(input("Digite o valor para depositar: "))
-        saldo = saldo + deposito
-        extrato += f"Deposito: R${str(deposito)} \n"
+        if deposito > 0: 
+            saldo = saldo + deposito
+            extrato += f"Deposito: R${deposito:.2f} \n"
+        else:
+            print("Operação falhou! O valor informado é inválido!")
     elif opcao == 2:
         saque = int(input("Valor do saque: "))
-        if saque > limite:
+        if saque < 0:
+            print("Operação falhou! O valor informado é inválido!")
+        elif saldo <= 0:
+            print("Saldo insuficiente!")
+        elif saque > limite:
             print("Desculpe, mas o limite para saque é de R$500.00")
         elif numero_saques >= LIMTE_SAQUE:
             print("Desculpe, mas o limite de saque diários são de 3 vezes.")
-        elif saldo <= 0:
-            print("Saldo insuficiente!")
         else: 
             print("Saque realizado com sucesso!")
             numero_saques += 1
             saldo = saldo - saque
-            extrato += f"Saque: R${str(saque)} \n"
+            extrato += f"Saque: R${saque:.2f} \n"
     elif opcao == 3:
-        print(extrato)
-        print(f"Total: R${saldo}")
-
+        print(f"""
+        \n########## EXTRATO ##########
+        \n{extrato}
+        Total: R${saldo:.2f}
+        \n##############################
+        """)
     else:
         print("Opcão invalida! Tente novamente!")
             
